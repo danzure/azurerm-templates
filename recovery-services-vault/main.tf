54fc254df327db053bdb9046101cfe781386dbe6
Daniel Powley <danielpowley92@outlook.com>
2025-04-10 07:44:09 +0100

    Updated cofiguration, removed vm&vnet solution

--- recovery-services-vault/main.tf
+++ recovery-services-vault/main.tf
@@ -1,7 +1,7 @@
 # create the resource group
-resource "azurerm_resource_group" "rsv_rg" {
+resource "azurerm_resource_group" "rsv_resource_group" {
   location = var.location
-  tags = var.tags
+  tags     = var.tags
 
   name = format("rg-%s-%s-%s-${var.instance}",
     local.generate_resource_name.envrionment,
@@ -11,32 +11,32 @@ resource "azurerm_resource_group" "rsv_rg" {
 }
 
 # create the recovery services vault
-resource "azurerm_recovery_services_vault" "rsv" {
-  resource_group_name = azurerm_resource_group.rsv_rg.name
-  location = var.location
-  tags = var.tags
+resource "azurerm_recovery_services_vault" "recovery_services_vault" {
+  resource_group_name = azurerm_resource_group.rsv_resource_group.name
+  location            = azurerm_resource_group.rsv_resource_group.location
   
   name = format("rsv-%s-%s-%s-${var.instance}",
     local.generate_resource_name.envrionment,
     local.generate_resource_name.workload,
     local.generate_resource_name.location
     )
-
   sku = var.rsv_sku
-  storage_mode_type = var.rsv_storage_mode 
-  soft_delete_enabled = var.rsv_soft_delete
-  immutability = var.rsv_immutability
-  public_network_access_enabled = var.rsv_network_access
-  cross_region_restore_enabled = var.rsv_region_restore
 
-  depends_on = [ azurerm_resource_group.rsv_rg ]
+  public_network_access_enabled = var.rsv_network_access
+  cross_region_restore_enabled  = var.rsv_region_restore
+  storage_mode_type             = var.rsv_storage_mode
+  soft_delete_enabled           = var.rsv_soft_delete
+  immutability                  = var.rsv_immutability
+  tags                          = var.tags
+
+  depends_on = [ azurerm_resource_group.rsv_resource_group ]
 }
 
-resource "azurerm_backup_policy_vm" "vm_daily_policy" {
-  resource_group_name = azurerm_resource_group.rsv_rg.name
-  recovery_vault_name = azurerm_recovery_services_vault.rsv.name
-  name = var.rsv_vm_backup_name
+resource "azurerm_backup_policy_vm" "vm_backup_policy" {
+  resource_group_name = azurerm_resource_group.rsv_resource_group.name
+  recovery_vault_name = azurerm_recovery_services_vault.recovery_services_vault.name
 
+  name = var.rsv_vm_backup_name
   timezone = var.backup_timezone
 
   backup {
@@ -50,6 +50,19 @@ resource "azurerm_backup_policy_vm" "vm_daily_policy" {
 
   retention_weekly {
     count = 1
-    weekdays = [ "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday" ]
+    weekdays = [ "Monday","Wednesday","Friday","Saturday"]
+  }
+
+  retention_monthly {
+    count = 7
+    weekdays = ["Sunday"]
+    weeks = [ "First", "Last" ]
+  }
+
+  retention_yearly {
+    count = 77
+    weekdays = ["Sunday"]
+    weeks = ["Last"]
+    months = [ "January" ]
   }
 }
